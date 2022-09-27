e79720b29c2909fc258196a7f2083b5f6acdd9c1
aaronsando <111078817+aaronsando@users.noreply.github.com>
2022-09-27 10:00:46 -0600

    Add files via upload

---
+++ Act_7/Templates/head.php
@@ -0,0 +1,12 @@
+<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>
+			Productos
+		</title>
+		<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
+		<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+		<style type="text/css">
+			aside{
+				height: 90vh;
+			}
+		</style>
